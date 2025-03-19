114f7090d2820287d83d7fa2149bebd71c146e2d
JvCorMo <jvcormo@gmail.com>
2025-03-19 14:28:50 +0100

    ver3

--- Empleado.java
+++ Empleado.java
@@ -5,4 +5,8 @@ public class Empleado {
     private int edad;
     private double sueldo;
 
+
+        void incrementarSueldo( int porcentajeIncremento){
+            this.sueldo += sueldo*(1+porcentajeIncremento/100.0);
+        }
 }
